0eb6328d6a03ce012742a93f458a5387951d7610
Kirill Zaborsky <avto.oil@gmail.com>
2021-01-13 23:08:55 +0700

    Initial Commit

---
+++ CardWar/CardWarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardWarApp.swift
+//  CardWar
+//
+//  Created by Кирилл Заборский on 13.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct CardWarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
